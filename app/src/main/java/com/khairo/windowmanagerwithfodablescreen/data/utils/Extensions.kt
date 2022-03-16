ffc1a5f1e3cd30dd58559f34809ce1207e88ce52
Mohammad khair <mohammad.alhumsi@ihrjobs.com>
2022-03-16 15:38:57 +0300

    add window library

--- app/src/main/java/com/khairo/windowmanagerwithfodablescreen/data/utils/Extensions.kt
+++ app/src/main/java/com/khairo/windowmanagerwithfodablescreen/data/utils/Extensions.kt
@@ -1,9 +1,11 @@
 package com.khairo.windowmanagerwithfodablescreen.data.utils
 
 import android.content.Context
+import android.graphics.Rect
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.window.layout.DisplayFeature
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -46,4 +48,42 @@ fun Snackbar.action(action: String, color: Int? = null, listener: (View) -> Unit
 
 fun String.log() {
     Timber.d(this)
+}
+
+fun View.getFeatureBoundsInWindow(
+    displayFeature: DisplayFeature,
+    includePadding: Boolean = true
+): Rect? {
+    // Adjust the location of the view in the window to be in the same coordinate space as the feature.
+    val viewLocationInWindow = IntArray(2)
+    getLocationInWindow(viewLocationInWindow)
+
+    // Intersect the feature rectangle in window with view rectangle to clip the bounds.
+    val viewRect = Rect(
+        viewLocationInWindow[0], viewLocationInWindow[1],
+        viewLocationInWindow[0] + width, viewLocationInWindow[1] + height
+    )
+
+    // Include padding if needed
+    if (includePadding) {
+        viewRect.left += paddingLeft
+        viewRect.top += paddingTop
+        viewRect.right -= paddingRight
+        viewRect.bottom -= paddingBottom
+    }
+
+    val featureRectInView = Rect(displayFeature.bounds)
+    val intersects = featureRectInView.intersect(viewRect)
+
+    // Checks to see if the display feature overlaps with our view at all
+    if ((featureRectInView.width() == 0 && featureRectInView.height() == 0) ||
+        !intersects
+    ) {
+        return null
+    }
+
+    // Offset the feature coordinates to view coordinate space start point
+    featureRectInView.offset(-viewLocationInWindow[0], -viewLocationInWindow[1])
+
+    return featureRectInView
 }
--- app/src/main/java/com/khairo/windowmanagerwithfodablescreen/ui/MainActivity.kt
+++ app/src/main/java/com/khairo/windowmanagerwithfodablescreen/ui/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.khairo.windowmanagerwithfodablescreen.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowLayoutInfo
+import com.khairo.windowmanagerwithfodablescreen.R
 import com.khairo.windowmanagerwithfodablescreen.data.models.CartModel
 import com.khairo.windowmanagerwithfodablescreen.data.models.CategoriesModel
 import com.khairo.windowmanagerwithfodablescreen.data.models.ItemModel
 import com.khairo.windowmanagerwithfodablescreen.data.utils.MarginDecoration
+import com.khairo.windowmanagerwithfodablescreen.data.utils.getFeatureBoundsInWindow
 import com.khairo.windowmanagerwithfodablescreen.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +28,7 @@ class MainActivity : AppCompatActivity(), ItemsAdapter.OnClickListener,
     CategoriesAdapter.OnClickListener, CartAdapter.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var windowInfoTracker: WindowInfoTracker
     private val mainViewModel: MainViewModel by viewModels()
 
     private lateinit var categoriesAdapter: CategoriesAdapter
@@ -29,6 +39,10 @@ class MainActivity : AppCompatActivity(), ItemsAdapter.OnClickListener,
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
+
+        onWindowLayoutInfoChange()
 
         binding.apply {
             viewModel = mainViewModel
@@ -46,6 +60,73 @@ class MainActivity : AppCompatActivity(), ItemsAdapter.OnClickListener,
             clearCart.setOnClickListener { clearAllCart() }
 
             pay.setOnClickListener { clearAllCart() }
+        }
+    }
+
+    private fun onWindowLayoutInfoChange() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                windowInfoTracker.windowLayoutInfo(this@MainActivity)
+                    .collect { value ->
+                        updateUI(value)
+                    }
+            }
+        }
+    }
+
+    private fun updateUI(newLayoutInfo: WindowLayoutInfo) {
+        if (newLayoutInfo.displayFeatures.isNotEmpty())
+            alignViewToFoldingFeatureBounds(newLayoutInfo)
+    }
+
+    private fun alignViewToFoldingFeatureBounds(newLayoutInfo: WindowLayoutInfo) {
+        val constraintLayout = binding.mainView
+        val set = ConstraintSet()
+        set.clone(constraintLayout)
+
+        val foldingFeature = newLayoutInfo.displayFeatures[0] as FoldingFeature
+        val bounds = binding.root.getFeatureBoundsInWindow(foldingFeature)
+
+        bounds?.let { rect ->
+            val horizontalFoldingFeatureHeight = (rect.bottom - rect.top).coerceAtLeast(1)
+            val verticalFoldingFeatureWidth = (rect.right - rect.left).coerceAtLeast(1)
+
+            set.constrainHeight(
+                R.id.folding_feature,
+                horizontalFoldingFeatureHeight
+            )
+            set.constrainWidth(
+                R.id.folding_feature,
+                verticalFoldingFeatureWidth
+            )
+
+            set.connect(
+                R.id.folding_feature, ConstraintSet.START,
+                ConstraintSet.PARENT_ID, ConstraintSet.START, 0
+            )
+            set.connect(
+                R.id.folding_feature, ConstraintSet.TOP,
+                ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0
+            )
+
+            if (foldingFeature.orientation == FoldingFeature.Orientation.VERTICAL) {
+                set.setMargin(R.id.folding_feature, ConstraintSet.START, rect.left)
+//                set.connect(
+//                    R.id.layout_change, ConstraintSet.END,
+//                    R.id.folding_feature, ConstraintSet.START, 0
+//                )
+            } else {
+                // FoldingFeature is Horizontal
+                set.setMargin(R.id.folding_feature, ConstraintSet.TOP, rect.top)
+//                set.connect(
+//                    R.id.layout_change, ConstraintSet.TOP,
+//                    R.id.folding_feature, ConstraintSet.BOTTOM, 0
+//                )
+            }
+
+            // Set the view to visible and apply constraints
+            set.setVisibility(R.id.folding_feature, View.VISIBLE)
+            set.applyTo(constraintLayout)
         }
     }
 
